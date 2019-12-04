f01b3cdbe372dc8400ee70ebda7fdd5f0c5941ce
Duke Le <dle12@uncc.edu>
2019-12-04 02:24:34 -0500

    adjust sizing and added seating.png

--- Seating/obj/Debug/MainWindow.g.i.cs
+++ Seating/obj/Debug/MainWindow.g.i.cs
@@ -40,6 +40,14 @@ namespace Seating {
     /// </summary>
     public partial class MainWindow : System.Windows.Window, System.Windows.Markup.IComponentConnector {
         
+        
+        #line 26 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.Image logo;
+        
+        #line default
+        #line hidden
+        
         private bool _contentLoaded;
         
         /// <summary>
@@ -68,6 +76,12 @@ namespace Seating {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            this.logo = ((System.Windows.Controls.Image)(target));
+            return;
+            }
             this._contentLoaded = true;
         }
     }
