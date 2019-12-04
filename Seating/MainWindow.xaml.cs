5cf5b1a964a9afc3233fada14da68a027e623014
Duke Le <dle12@uncc.edu>
2019-12-04 12:29:03 -0500

    with airplane model

--- Seating/MainWindow.xaml.cs
+++ Seating/MainWindow.xaml.cs
@@ -23,6 +23,63 @@ namespace Seating
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow.Height = 300;
+            mainWindow.Width = 550;
+        }
+
+        private void FindSeat(String seatClass, int numPass, String seatPref)
+        {
+            int[,] firstArray = new int[4, 3];
+            int[,] economyArray = new int[14, 5];
+
+        }
+
+
+        private void addPassengersButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Height = 550;
+            mainWindow.Width = 550;
+
+        }
+
+        private void quitButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Close();
+        }
+
+        public void classDrop_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (classDrop.SelectedIndex == 0)
+            {
+                passengersDrop.Items.Clear();
+                prefDrop.Items.Clear();
+                passengersDrop.Items.Add("1");
+                passengersDrop.Items.Add("2");
+                prefDrop.Items.Add("Aisle");
+                prefDrop.Items.Add("Window");
+            }
+            if (classDrop.SelectedIndex == 1)
+            {
+                passengersDrop.Items.Clear();
+                prefDrop.Items.Clear();
+                passengersDrop.Items.Add("1");
+                passengersDrop.Items.Add("2");
+                passengersDrop.Items.Add("3");
+                prefDrop.Items.Add("Center");
+                prefDrop.Items.Add("Window");
+            }
+        }
+
+        private void showSeatingButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Width = 780;
+            mainWindow.Height = 530;
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            showSeatingButton_Click(sender, e);
         }
     }
+
 }
