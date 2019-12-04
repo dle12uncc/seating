4b96a9cfdf84da86f7d92ddc1cddbf13118d57f2
Duke Le <dle12@uncc.edu>
2019-12-04 02:17:29 -0500

    added buttons and changed logo

--- Seating/obj/Debug/Seating_Content.g.i.cs
+++ Seating/obj/Debug/Seating_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("background.png")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("logo.png")]
 
 
